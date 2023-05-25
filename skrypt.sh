204830ebcd11ab246a69c0d7cdac8697942e02d9
nashkevich <agz-2001@mail.ru>
2023-05-26 00:11:03 +0200

    add new flags

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 case $1 in
---logs) for (( i=1; i<=100; i++ ))
+--log | -l) for (( i=1; i<=100; i++ ))
 do
   touch file_$i.txt
 done;;
---date) date +"%m-%d-%y";;
---logs30) echo "Hom many files you want?"
+--date | -d) date +"%m-%d-%y";;
+--logs30 | -l30) echo "Hom many files you want?"
 read num;
 for (( a=1; a<=num; a++ ))
 do
   touch newfile_$a.txt
 done;;
---help) echo "All cases you have: --date , --log , log30"
+--help | -h) echo "All cases you have: --date , --log , log30"
 esac
