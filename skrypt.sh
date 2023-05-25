76ce0a1d3fcfdc5e57bb41c43a0879f8785f9eb3
nashkevich <agz-2001@mail.ru>
2023-05-25 23:23:45 +0200

    add 2 flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-<<<<<<< HEAD
-for (( i=1; i<=100; i++ ))
+case $1 in
+--logs) for (( i=1; i<=100; i++ ))
 do
   touch file_$i.txt
-done 
-=======
-case $1 in
---date) date +"%m-%d-%y"
-esac 
->>>>>>> date
+done;;
+--date) date +"%m-%d-%y";;
+esac
